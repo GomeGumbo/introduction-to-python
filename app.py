fdf805423705e63a6a7503e23da265fb921a1d5a
Gome Gumbo <gomegumbo16@gmail.com>
2025-01-01 21:07:23 +0200

    initialise files

---
+++ app.py
@@ -0,0 +1,10 @@
+import utils
+from utils import Utils
+
+numbers = [2,5,1,10]
+maximum = numbers[0]
+
+
+for number in numbers:
+    my_function = Utils(max_mum=maximum, number=number)
+
---
+++ utils.py
@@ -0,0 +1,14 @@
+class Utils:
+    def __init__(self,max_mum,number):
+        self.max_num = max_mum
+        self.number = number
+
+
+    def find_max (self):
+        int(self.max_num)
+        int(self.number)
+        if self.number > self.max_num:
+            self.max_num = self.number
+            return self.max_num
+
+
